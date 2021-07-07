2af7487985ea4cd545e265fd276286931990f18d
Preetha Appan <preetha@hashicorp.com>
2021-07-07 12:32:01 -0500

    Fix formatting

--- version/version_test.go
+++ version/version_test.go
@@ -1,13 +1,13 @@
 package version
 
 import (
-	"testing"
 	"github.com/stretchr/testify/require"
+	"testing"
 )
 
 func TestVersion(t *testing.T) {
 	version := GetHumanVersion()
 	req := require.New(t)
-    req.NotEmpty(version, "Version cannot be empty")
+	req.NotEmpty(version, "Version cannot be empty")
 
 }
