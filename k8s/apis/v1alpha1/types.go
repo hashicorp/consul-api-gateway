9a5a82d9435738a5bdf0239da6df59821f5cccdb
Andrew Stucki <andrew.stucki@hashicorp.com>
2021-09-03 23:08:37 -0400

    Fix up CRD for gateway class

--- k8s/apis/v1alpha1/types.go
+++ k8s/apis/v1alpha1/types.go
@@ -8,43 +8,63 @@ import (
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// Config describes the configuration of a Polar GatewayClass.
+// GatewayClassConfig describes the configuration of a Polar GatewayClass.
 // +k8s:openapi-gen=true
 // +resource:path=resources
-type Config struct {
+type GatewayClassConfig struct {
 	metav1.TypeMeta `json:",inline"`
 	// Standard object's metadata.
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec ConfigSpec `json:"spec,omitempty"`
+	Spec GatewayClassConfigSpec `json:"spec,omitempty"`
 }
 
-// ConfigSpec specifies the 'spec' of the Config CRD.
-type ConfigSpec struct {
+// GatewayClassConfigSpec specifies the 'spec' of the Config CRD.
+type GatewayClassConfigSpec struct {
 	// +kubebuilder:validation:Enum=ClusterIP;NodePort;LoadBalancer
-	ServiceType corev1.ServiceType `json:"serviceType,omitempty"`
+	ServiceType  corev1.ServiceType  `json:"serviceType,omitempty"`
+	NodeSelector corev1.NodeSelector `json:"nodeSelector,omitempty"`
 	// If this is set, then the Envoy container ports are mapped
 	// to host ports.
 	UseHostPorts bool `json:"useHostPorts,omitempty"`
 	// Configuration information about connecting to Consul.
 	ConsulSpec ConsulSpec `json:"consul,omitempty"`
+	// SDS server specific configuration
+	SDSServerSpec SDSServerSpec `json:"sds,omitempty"`
 	// Configuration information about the images to use
 	ImageSpec ImageSpec `json:"image,omitempty"`
+	// +kubebuilder:validation:Enum=trace;debug;info;warning;error
+	// Logging levels
+	LogLevel string `json:"logLevel,omitempty"`
+}
+
+type SDSServerSpec struct {
+	// The SDS server address for Envoy secrets
+	Address string `json:"address,omitempty"`
+	// The SDS server port for Envoy secrets
+	Port int `json:"port,omitempty"`
 }
 
 type ConsulSpec struct {
-	// The Consul auth method used for initial authentication by Polar.
-	Method string `json:"method,omitempty"`
-	// The Kubernetes service account to authenticate as.
-	Account string `json:"account,omitempty"`
+	// Consul authentication information
+	AuthSpec AuthSpec `json:"authentication,omitempty"`
 	// The scheme to use for connecting to Consul.
 	Scheme string `json:"scheme,omitempty"`
 	// The address of the consul server to communicate with in the gateway
 	// pod. If not specified, the pod will attempt to use a local agent on
 	// the host on which it is running.
 	Address string `json:"address,omitempty"`
+	// The information about Consul's ports
+	PortSpec PortSpec `json:"ports,omitempty"`
 	// The location of a secret to mount with the Consul root CA.
 	CASecret string `json:"caSecret,omitempty"`
+}
+
+type PortSpec struct {
+	// The port for Consul's HTTP server.
+	HTTP int `json:"http,omitempty"`
+	// The grpc port for Consul's xDS server.
+	GRPC int `json:"grpc,omitempty"`
 }
 
 type ImageSpec struct {
@@ -54,12 +74,21 @@ type ImageSpec struct {
 	Envoy string `json:"envoy,omitempty"`
 }
 
+type AuthSpec struct {
+	// The Consul auth method used for initial authentication by Polar.
+	Method string `json:"method,omitempty"`
+	// The Kubernetes service account to authenticate as.
+	Account string `json:"account,omitempty"`
+	// The Consul namespace to use for authentication.
+	Namespace string `json:"namespace,omitempty"`
+}
+
 //+kubebuilder:object:root=true
 
-// ConfigList is a list of Config resources.
-type ConfigList struct {
+// GatewayClassConfigList is a list of Config resources.
+type GatewayClassConfigList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata"`
 
-	Items []Config `json:"items"`
+	Items []GatewayClassConfig `json:"items"`
 }
