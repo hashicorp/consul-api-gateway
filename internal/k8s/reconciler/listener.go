945a1e4fc05421951b31326410e954ca8c18c649
Mike Morris <mikemorris@users.noreply.github.com>
2022-01-14 14:00:52 -0500

    k8s/reconciler: remove SHA-1 cipher suites

--- internal/k8s/reconciler/listener.go
+++ internal/k8s/reconciler/listener.go
@@ -209,21 +209,26 @@ var tlsVersionsWithConfigurableCipherSuites = map[string]struct{}{
 //
 // NOTE: excludes the following cipher suites which are currently supported by Envoy
 // but insecure and pending removal
+//
+// https://github.com/envoyproxy/envoy/issues/5399
 // TLS_RSA_WITH_AES_128_GCM_SHA256
 // TLS_RSA_WITH_AES_128_CBC_SHA
 // TLS_RSA_WITH_AES_256_GCM_SHA384
 // TLS_RSA_WITH_AES_256_CBC_SHA
+//
+// https://github.com/envoyproxy/envoy/issues/5400
+// TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA
+// TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA
+// TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA
+// TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA
+//
 var supportedTlsCipherSuites = map[string]struct{}{
 	"TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256":       {},
 	"TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256": {},
 	"TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256":         {},
 	"TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256":   {},
-	"TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA":          {},
-	"TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA":            {},
 	"TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384":       {},
 	"TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384":         {},
-	"TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA":          {},
-	"TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA":            {},
 }
 
 func (l *K8sListener) validateUnsupported() {
