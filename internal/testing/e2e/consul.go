29cdefacde05ff0495dbffba2292d8b3ff3c11b1
Andrew Stucki <andrew.stucki@hashicorp.com>
2022-02-16 13:01:39 -0500

    More enterprise policy changes

--- internal/testing/e2e/consul.go
+++ internal/testing/e2e/consul.go
@@ -609,9 +609,9 @@ func adminPolicy() *api.ACLPolicy {
 	acl = "write"
 	policy = "write"
 	service_prefix "" { policy = "write" }
-	agent_prefix "" { policy = "write" }
 	session_prefix "" { policy = "write" }
 	}
+	agent_prefix "" { policy = "write" }
 	node_prefix "" { policy = "write" }
 	event_prefix "" { policy = "write" }
 	query_prefix "" { policy = "write" }
