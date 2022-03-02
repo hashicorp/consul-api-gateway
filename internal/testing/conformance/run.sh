3fba50a31b50e257fbc54104c556648ef6718baa
Nathan Coleman <nathan.coleman@hashicorp.com>
2022-03-02 15:02:07 -0500

    Add minimal kustomize patch for upstream conformance testing resources

---
+++ internal/testing/conformance/run.sh
@@ -0,0 +1,14 @@
+# Make sure the Kubernetes cluster that you're running against has Consul + Gateway API CRDs installed
+rm -rf gateway-api
+git clone --depth 1 git@github.com:kubernetes-sigs/gateway-api
+cp kustomization.yaml proxydefaults.yaml gateway-api/conformance/
+cd gateway-api/conformance/
+kubectl kustomize ./ --output ./base/manifests.yaml
+
+# TODO Right now, the conformance tests don't pass because they assume the route will work
+#   as soon as the HTTPRoute has parent(s) listed; however, we sync resources into Consul/Envoy
+#   and don't actually know when the route is available.
+#   For now, we can only get the tests passing by installing the resources for a test and giving them
+#   time to "settle" before running the corresponding test. This requires manual changes and can't
+#   be automated in a sensible fashion.
+go test ./ --gateway-class consul-api-gateway
