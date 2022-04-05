4cd256f453ca27a060d693842ac5d8e28ab7fa49
Nathan Coleman <nathan.coleman@hashicorp.com>
2022-04-05 19:39:27 -0400

    Add test coverage for HTTPRoute validator

--- internal/k8s/reconciler/utils_test.go
+++ internal/k8s/reconciler/utils_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	core "k8s.io/api/core/v1"
 	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -12,8 +13,9 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	gw "sigs.k8s.io/gateway-api/apis/v1alpha2"
 
-	"github.com/hashicorp/consul-api-gateway/internal/k8s/gatewayclient/mocks"
 	"github.com/hashicorp/go-hclog"
+
+	"github.com/hashicorp/consul-api-gateway/internal/k8s/gatewayclient/mocks"
 )
 
 func TestRouteMatchesListener(t *testing.T) {
@@ -358,4 +360,108 @@ func TestGatewayStatusEqual(t *testing.T) {
 	require.False(t, gatewayStatusEqual(gw.GatewayStatus{}, gw.GatewayStatus{
 		Listeners: []gw.ListenerStatus{{}},
 	}))
+}
+
+func TestRouteAllowedForHTTPBackend(t *testing.T) {
+	type testCase struct {
+		name         string
+		routeNS      string
+		backendNS    string
+		backendName  string
+		policyFromNS string
+		policyToName *string
+		allowed      bool
+	}
+
+	ns1, ns2, ns3 := "namespace1", "namespace2", "namespace3"
+	backend1, backend2, backend3 := "backend1", "backend2", "backend3"
+
+	for _, tc := range []testCase{
+		{name: "same-namespace-no-name-allowed", routeNS: ns1, backendNS: ns1, backendName: backend1, policyFromNS: ns1, policyToName: nil, allowed: true},
+		{name: "same-namespace-with-name-allowed", routeNS: ns1, backendNS: ns1, backendName: backend1, policyFromNS: ns1, policyToName: &backend1, allowed: true},
+		{name: "different-namespace-no-name-allowed", routeNS: ns1, backendNS: ns2, backendName: backend2, policyFromNS: ns1, policyToName: nil, allowed: true},
+		{name: "different-namespace-with-name-allowed", routeNS: ns1, backendNS: ns2, backendName: backend2, policyFromNS: ns1, policyToName: &backend2, allowed: true},
+		{name: "mismatched-policy-from-namespace-disallowed", routeNS: ns1, backendNS: ns2, backendName: backend2, policyFromNS: ns3, policyToName: &backend2, allowed: false},
+		{name: "mismatched-policy-to-name-disallowed", routeNS: ns1, backendNS: ns2, backendName: backend2, policyFromNS: ns1, policyToName: &backend3, allowed: false},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			client := mocks.NewMockClient(ctrl)
+
+			group := gw.Group("")
+			kind := gw.Kind("Service")
+			namespace := gw.Namespace(tc.backendNS)
+
+			backendRef := gw.HTTPBackendRef{
+				BackendRef: gw.BackendRef{
+					BackendObjectReference: gw.BackendObjectReference{
+						Group:     &group,
+						Kind:      &kind,
+						Name:      gw.ObjectName(tc.backendName),
+						Namespace: &namespace,
+					},
+				},
+			}
+
+			route := &gw.HTTPRoute{
+				ObjectMeta: meta.ObjectMeta{Namespace: tc.routeNS},
+				TypeMeta:   meta.TypeMeta{APIVersion: "gateway.networking.k8s.io/v1alpha2", Kind: "HTTPRoute"},
+				Spec: gw.HTTPRouteSpec{
+					Rules: []gw.HTTPRouteRule{{
+						BackendRefs: []gw.HTTPBackendRef{backendRef},
+					}},
+				},
+			}
+
+			var toName *gw.ObjectName
+			if tc.policyToName != nil {
+				on := gw.ObjectName(*tc.policyToName)
+				toName = &on
+			}
+
+			if tc.routeNS != tc.backendNS {
+				referencePolicy := gw.ReferencePolicy{
+					TypeMeta:   meta.TypeMeta{},
+					ObjectMeta: meta.ObjectMeta{Namespace: tc.backendNS},
+					Spec: gw.ReferencePolicySpec{
+						From: []gw.ReferencePolicyFrom{{
+							Group:     "gateway.networking.k8s.io",
+							Kind:      "HTTPRoute",
+							Namespace: gw.Namespace(tc.policyFromNS),
+						}},
+						To: []gw.ReferencePolicyTo{{
+							Group: "",
+							Kind:  "Service",
+							Name:  toName,
+						}},
+					},
+				}
+
+				throwawayPolicy := gw.ReferencePolicy{
+					ObjectMeta: meta.ObjectMeta{Namespace: tc.backendNS},
+					Spec: gw.ReferencePolicySpec{
+						From: []gw.ReferencePolicyFrom{{
+							Group:     "Kool & The Gang",
+							Kind:      "Jungle Boogie",
+							Namespace: "Wild And Peaceful",
+						}},
+						To: []gw.ReferencePolicyTo{{
+							Group: "does not exist",
+							Kind:  "does not exist",
+							Name:  nil,
+						}},
+					},
+				}
+
+				client.EXPECT().
+					GetReferencePoliciesInNamespace(gomock.Any(), tc.backendNS).
+					Return([]gw.ReferencePolicy{throwawayPolicy, referencePolicy}, nil)
+			}
+
+			allowed, err := routeAllowedForHTTPBackend(context.Background(), route, backendRef, client)
+			require.NoError(t, err)
+			assert.Equal(t, tc.allowed, allowed)
+		})
+	}
 }
