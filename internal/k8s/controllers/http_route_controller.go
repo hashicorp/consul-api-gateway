c45e43591a9af81b847fcb79617e3ee0c4d78f11
Mike Morris <mikemorris@users.noreply.github.com>
2022-04-20 13:46:37 -0400

    refpolicy: add ReferencePolicy watch and draft of filtering logic to HTTPRouteController

--- internal/k8s/controllers/http_route_controller.go
+++ internal/k8s/controllers/http_route_controller.go
@@ -3,7 +3,15 @@ package controllers
 import (
 	"context"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/selection"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 	gateway "sigs.k8s.io/gateway-api/apis/v1alpha2"
 
 	"github.com/hashicorp/consul-api-gateway/internal/k8s/gatewayclient"
@@ -53,5 +61,143 @@ func (r *HTTPRouteReconciler) Reconcile(ctx context.Context, req ctrl.Request) (
 func (r *HTTPRouteReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&gateway.HTTPRoute{}).
+		Watches(
+			// TODO: how does this handle deletes?
+			&source.Kind{Type: &gateway.ReferencePolicy{}},
+			handler.EnqueueRequestsFromMapFunc(r.referencePolicyToRouteRequests),
+		).
 		Complete(gatewayclient.NewRequeueingMiddleware(r.Log, r))
+}
+
+func getReferencePolicyObjectsFrom(refPolicy gateway.ReferencePolicy) []client.Object {
+	matches := []client.Object{}
+
+	for _, from := range refPolicy.Spec.From {
+		matchLabels := map[string]string{
+			"kubernetes.io/metadata.kind":      string(from.Kind),
+			"kubernetes.io/metadata.namespace": string(from.Namespace),
+		}
+
+		if from.Group != "" {
+			matchLabels["kubernetes.io/metadata.group"] = string(from.Group)
+		} else {
+			// When empty, the Kubernetes core API group is inferred.
+			matchLabels["kubernetes.io/metadata.group"] = "core/v1"
+		}
+
+		selector := metav1.LabelSelector{
+			MatchExpressions: []metav1.LabelSelectorRequirement{{
+				Key:      "",
+				Operator: "In",
+				Values:   []string{},
+			}},
+			MatchLabels: matchLabels,
+		}
+
+		matches = append(matches, []client.Object{}...)
+	}
+
+	return matches
+}
+
+func getReferencePolicyObjectsTo(refPolicy gateway.ReferencePolicy) []client.Object {
+	matches := []client.Object{}
+
+	for _, to := range refPolicy.Spec.To {
+		selector := labels.NewSelector()
+
+		kindReq, _ := labels.NewRequirement(
+			"kubernetes.io/metadata.kind",
+			selection.In,
+			[]string{string(to.Kind)},
+		)
+
+		namespaceReq, _ := labels.NewRequirement(
+			"kubernetes.io/metadata.namesapce",
+			selection.In,
+			[]string{refPolicy.Namespace},
+		)
+
+		selector = selector.Add(*kindReq, *namespaceReq)
+
+		var groupReq *labels.Requirement
+		if to.Group != "" {
+			groupReq, _ = labels.NewRequirement(
+				"kubernetes.io/metadata.group",
+				selection.In,
+				[]string{string(to.Group)},
+			)
+		} else {
+			// When empty, the Kubernetes core API group is inferred.
+			groupReq, _ = labels.NewRequirement(
+				"kubernetes.io/metadata.group",
+				selection.In,
+				[]string{"core/v1"},
+			)
+		}
+
+		selector = selector.Add(*groupReq)
+
+		if to.Name != nil {
+			nameReq, _ := labels.NewRequirement(
+				"kubernetes.io/metadata.name",
+				selection.In,
+				[]string{string(*to.Name)},
+			)
+			selector = selector.Add(*nameReq)
+		}
+
+		// TODO: use selector
+		matches = append(matches, []client.Object{}...)
+	}
+
+	return matches
+}
+
+func GetRoutesAffectedByReferencePolicy(refPolicy gateway.ReferencePolicy) []gateway.HTTPRoute {
+	matches := []gateway.HTTPRoute{}
+	objectsTo := getReferencePolicyObjectsTo(refPolicy)
+
+	// FIXME: Only checking Routes selected by GetReferencePolicyObjectsFrom isn't
+	// enough, need to reconcile Routes which may have been allowed before but no
+	// longer are permitted
+	//
+	// GET Routes WHERE BackendRef is selected by GetReferencePolicyObjectsTo
+	for _, route := range getReferencePolicyObjectsFrom(refPolicy) {
+		// TODO: should this use reflection to handle xRoute types? seems expensive
+		for _, rules := range route.Spec.Rules {
+			for _, backendRef := range rules.BackendRefs {
+				for _, from := range objectsTo {
+					if backendRef.DeepEqual(from) {
+						matches = append(matches, backendRef)
+					}
+				}
+			}
+		}
+	}
+
+	return matches
+}
+
+func (r *HTTPRouteReconciler) referencePolicyToRouteRequests(object client.Object) []reconcile.Request {
+	r.Log.Info("event for ReferencePolicy", "object", object)
+
+	refPolicy := gateway.ReferencePolicy{}
+	routes := GetRoutesAffectedByReferencePolicy(refPolicy)
+	requests := []reconcile.Request{}
+
+	for _, route := range routes {
+		requests = append(requests, reconcile.Request{
+			NamespacedName: types.NamespacedName{
+				Name:      route.Name,
+				Namespace: route.Namespace,
+			},
+		})
+	}
+
+	if len(requests) > 0 {
+		return requests
+	}
+
+	return nil
 }
