313d7ad88b61c7fa0bc605a762b3c32b12b41bdb
Mike Morris <mikemorris@users.noreply.github.com>
2022-04-27 10:56:42 -0400

    changelog: add go-changelog templates and tooling (#101)
    * changelog: add go-changelog templates
    
    * ci: add changelog-checker from Consul core
    
    * github: update PR template to remove HashiCorp-only changelog note
    
    * makefile: add stub changelog tasks
    
    * changelog: remove provider-specific types from release note template
    
    * changelog: remove provider-specific features section from changelog template
    
    * changelog: restore FEATURES section in template, move BREAKING CHANGES above DEPRECATIONS
    
    * makefile: implement make changelog task
    
    * makefile: add stub for make changelog-entry
    
    * attempt to call changelog-entry from tmp fork
    
    * fixme: use go-changelog fork until changelog-entry PR is merged
    
    * changelog: add types file, add security section and reorder changelog template, update changelog-entry make task to main branch
    
    * fixme: update make changelog-check task to install from PR branch, not use types file yet
    
    * ci: validate changelog entry content with changelog-check
    
    * fixup: move changelog-check.sh to scripts directory
    
    * ci: set goenv 1.16 to build changelog-check from source
    
    * ci: minor cleanup and naming for changelog-check GitHub Action
    
    * ci: rename workflow file to changelog.yml
    
    * changelog: add line breaks after section headers in template
    
    * changelog: fix note template link, add underscore prefix handling for omitting link
    
    * ci: use extglob syntax to match optional underscore for enterprise changelog entries and one or more digits
    
    * cleanup: remove duplicate changelog-check.sh
    
    * github: add PR template note to use make changelog-check

--- .github/pull_request_template.md
+++ .github/pull_request_template.md
@@ -7,5 +7,6 @@
 ### Checklist:
 - [ ] Tests added
 - [ ] CHANGELOG entry added 
-  > HashiCorp engineers only, community PRs should not add a changelog entry.
+  > Run `make changelog-entry` for guidance in authoring a changelog entry, and
+  > commit the resulting file, which should have a name matching your PR number.
   > Entries should use imperative present tense (e.g. Add support for...)
---
+++ scripts/changelog-check.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+shopt -s extglob
+
+changelog_file_path=$1
+
+if [ ! -z "$2" ]; then
+  enforce_matching_pull_request_number="matching this PR number "
+fi
+
+# Check if there is a diff matching the expected changelog file path
+changelog_files=$(git --no-pager diff --name-only HEAD "$(git merge-base HEAD "origin/main")" -- ${changelog_file_path})
+
+# Exit with error if no changelog entry is found
+if [ -z "$changelog_files" ]; then
+  echo "Did not find a changelog entry ${enforce_matching_pull_request_number}and the 'pr/no-changelog' label was not applied. Reference - https://github.com/hashicorp/consul/pull/8387"
+  exit 1
+fi
+
+# Validate format with make changelog-check, exit with error if any note has an
+# invalid format
+for file in $changelog_files; do
+  if ! cat $file | make changelog-check; then
+    echo "Found a changelog entry ${enforce_matching_pull_request_number}but the note format in ${file} was invalid."
+    exit 1
+  fi
+done
+
+echo "Found valid changelog entry!"
