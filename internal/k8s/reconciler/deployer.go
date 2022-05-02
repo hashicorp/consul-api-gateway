b4dcd41adfe79f5bcf71ae523c87d7eed54f13c7
Andrew Stucki <andrew.stucki@hashicorp.com>
2022-05-02 14:17:39 -0400

    Update internal/k8s/reconciler/deployer.go
    Co-authored-by: Nathan Coleman <nathan.coleman@hashicorp.com>

--- internal/k8s/reconciler/deployer.go
+++ internal/k8s/reconciler/deployer.go
@@ -87,23 +87,27 @@ func (d *GatewayDeployer) ensureDeployment(ctx context.Context, namespace string
 }
 
 func (d *GatewayDeployer) ensureService(ctx context.Context, config apigwv1alpha1.GatewayClassConfig, gateway *gw.Gateway) error {
-	if service := d.Service(config, gateway); service != nil {
-		mutated := service.DeepCopy()
-		if updated, err := d.client.CreateOrUpdateService(ctx, mutated, func() error {
-			mutated = apigwv1alpha1.MergeService(service, mutated)
-			return d.client.SetControllerOwnership(gateway, mutated)
-		}); err != nil {
-			return fmt.Errorf("failed to create or update gateway service: %w", err)
-		} else if updated {
-			if d.logger.IsTrace() {
-				data, err := json.MarshalIndent(mutated, "", "  ")
-				if err == nil {
-					d.logger.Trace("created or updated gateway service", "service", string(data))
-				}
-			}
+	service := d.Service(config, gateway)
+	if service == nil {
+	    return nil
+	}
+	
+	mutated := service.DeepCopy()
+	updated, err := d.client.CreateOrUpdateService(ctx, mutated, func() error {
+		mutated = apigwv1alpha1.MergeService(service, mutated)
+		return d.client.SetControllerOwnership(gateway, mutated)
+	})
+	if err != nil {
+		return fmt.Errorf("failed to create or update gateway service: %w", err)
+	}
+	
+	if updated && d.logger.IsTrace() {
+		data, err := json.MarshalIndent(mutated, "", "  ")
+		if err == nil {
+			d.logger.Trace("created or updated gateway service", "service", string(data))
 		}
 	}
-
+	
 	return nil
 }
 
