7f93cafc9e7f11478128e9cc833a4fc27fbee0b1
Nathan Coleman <nathan.coleman@hashicorp.com>
2022-06-24 17:05:04 -0400

    Update mock gatewayclient

--- internal/k8s/gatewayclient/mocks/gatewayclient.go
+++ internal/k8s/gatewayclient/mocks/gatewayclient.go
@@ -289,6 +289,21 @@ func (mr *MockClientMockRecorder) GetHTTPRoute(ctx, key interface{}) *gomock.Cal
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetHTTPRoute", reflect.TypeOf((*MockClient)(nil).GetHTTPRoute), ctx, key)
 }
 
+// GetHTTPRoutes mocks base method.
+func (m *MockClient) GetHTTPRoutes(ctx context.Context) ([]v1alpha2.HTTPRoute, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetHTTPRoutes", ctx)
+	ret0, _ := ret[0].([]v1alpha2.HTTPRoute)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// GetHTTPRoutes indicates an expected call of GetHTTPRoutes.
+func (mr *MockClientMockRecorder) GetHTTPRoutes(ctx interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetHTTPRoutes", reflect.TypeOf((*MockClient)(nil).GetHTTPRoutes), ctx)
+}
+
 // GetHTTPRoutesInNamespace mocks base method.
 func (m *MockClient) GetHTTPRoutesInNamespace(ctx context.Context, ns string) ([]v1alpha2.HTTPRoute, error) {
 	m.ctrl.T.Helper()
