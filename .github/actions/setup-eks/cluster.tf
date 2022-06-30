977fe7333ec707d8af8a103c1a4de73bc0b24ef8
Nathan Coleman <nathan.coleman@hashicorp.com>
2022-06-30 16:34:00 -0400

    Simplify EKS tf based on Learn guide

--- .github/actions/setup-eks/cluster.tf
+++ .github/actions/setup-eks/cluster.tf
@@ -10,22 +10,15 @@ variable "region" {
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
 }
 
 provider "aws" {
   region = var.region
 }
 
-data "aws_availability_zones" "available" {
-  state = "available"
-
-  filter {
-    name   = "opt-in-status"
-    values = ["opt-in-not-required"]
-  }
-}
+data "aws_availability_zones" "available" {}
 
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
@@ -44,23 +37,32 @@ module "vpc" {
   azs              = data.aws_availability_zones.available.names
   private_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets   = ["10.0.4.0/24", "10.0.5.0/24"]
-
-  manage_default_route_table = true
-  default_route_table_tags   = { DefaultRouteTable = true }
-
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
-  enable_dns_support   = true
+
+  tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
 }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "17.22.0"
-
+  version = "17.24.0"
   cluster_name    = var.cluster_name
-  cluster_version = "1.21"
-  subnets         = module.vpc.public_subnets
+  cluster_version = "1.22"
+  subnets         = module.vpc.private_subnets
+
   vpc_id          = module.vpc.vpc_id
 
   node_groups = {
