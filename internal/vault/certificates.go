74e945c765f74b0de42c2a885d9b4d2c1bb8d13e
Nathan Coleman <nathan.coleman@hashicorp.com>
2022-07-27 14:00:33 -0400

    Iterate on vault.Secret + vault.SecretClient

--- internal/vault/certificates.go
+++ internal/vault/certificates.go
@@ -4,13 +4,20 @@ import (
 	"context"
 	"time"
 
+	"github.com/armon/go-metrics"
 	tls "github.com/envoyproxy/go-control-plane/envoy/extensions/transport_sockets/tls/v3"
 	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/vault/api"
+
+	gwMetrics "github.com/hashicorp/consul-api-gateway/internal/metrics"
 )
 
 const SecretScheme = "vault"
 
+// SecretClient acts as a secret fetcher for Vault.
+//
+// This Vault-specific implementation corresponds with the K8s-specific
+// implementation, k8s.K8sSecretClient.
 type SecretClient struct {
 	logger hclog.Logger
 	client *api.Client
@@ -29,6 +36,20 @@ func NewSecretClient(logger hclog.Logger, config *api.Config) (*SecretClient, er
 }
 
 func (c *SecretClient) FetchSecret(ctx context.Context, fullName string) (*tls.Secret, time.Time, error) {
-	//TODO implement me
-	panic("implement me")
+	parsedSecret, err := ParseSecret(fullName)
+	if err != nil {
+		return nil, time.Time{}, err
+	}
+
+	c.logger.Trace("fetching SDS secret", "name", fullName)
+	gwMetrics.Registry.IncrCounterWithLabels(gwMetrics.SDSCertificateFetches, 1, []metrics.Label{
+		{Name: "fetcher", Value: "vault"},
+		{Name: "name", Value: parsedSecret.String()}}) // TODO Use fullName instead of serializing again
+
+	// Retrieve certificate + key from Vault
+
+	// Convert to *tls.Secret
+
+	// Return *tls.Secret, x509.Certificate.NotAfter
+	return &tls.Secret{}, time.Now(), nil
 }
--- internal/vault/secrets.go
+++ internal/vault/secrets.go
@@ -1,4 +1,58 @@
 package vault
 
-// Secret is a wrapper to a certificate secret stored in Vault
-type Secret struct{}
+import (
+	"errors"
+	"fmt"
+	"net/url"
+	"strings"
+)
+
+var (
+	ErrInvalidSecret = errors.New("invalid vault secret")
+)
+
+// Secret is a wrapper to a certificate secret stored in Vault.
+//
+// This Vault-specific implementation corresponds with the K8s-specific
+// implementation, utils.K8sSecret.
+type Secret struct {
+	Issuer string
+	Issue  string
+}
+
+func NewSecret(issuer, issue string) Secret {
+	return Secret{
+		Issuer: issuer,
+		Issue:  issue,
+	}
+}
+
+func ParseSecret(ref string) (Secret, error) {
+	parsed, err := url.Parse(ref)
+	if err != nil {
+		return Secret{}, err
+	}
+
+	if parsed.Scheme != SecretScheme {
+		return Secret{}, ErrInvalidSecret
+	}
+
+	if !strings.HasPrefix(parsed.Path, "/") {
+		return Secret{}, ErrInvalidSecret
+	}
+
+	ref = strings.TrimPrefix(parsed.Path, "/")
+	tokens := strings.SplitN(ref, "/", 2)
+	if len(tokens) != 2 {
+		return Secret{}, ErrInvalidSecret
+	}
+
+	return NewSecret(tokens[0], tokens[1]), nil
+}
+
+func (s Secret) String() string {
+	return (&url.URL{
+		Scheme: SecretScheme,
+		Path:   fmt.Sprintf("/%s/%s", s.Issuer, s.Issue),
+	}).String()
+}
