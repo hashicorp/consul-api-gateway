25185fa946fd00172ab916087f68b202d5c48191
Andrew Stucki <andrew.stucki@hashicorp.com>
2022-09-02 23:48:33 -0400

    Add stubs for gateway client

---
+++ internal/api/v1/client.go
@@ -0,0 +1,44 @@
+package v1
+
+import (
+	"errors"
+
+	"github.com/deepmap/oapi-codegen/pkg/securityprovider"
+)
+
+// TODO(andrew): most of this is boilerplate that should be generated
+
+const serverBase = "/api/v1"
+
+type ClientConfig struct {
+	Server string
+	Base   string
+	Token  string
+}
+
+type APIClient struct {
+	client *ClientWithResponses
+}
+
+func CreateClient(config ClientConfig) (*APIClient, error) {
+	if config.Server == "" {
+		return nil, errors.New("must specify a server value")
+	}
+	clientOptions := []ClientOption{}
+
+	if config.Token != "" {
+		apiKeyProvider, _ := securityprovider.NewSecurityProviderApiKey("header", "X-Consul-Token", config.Token)
+		clientOptions = append(clientOptions, WithRequestEditorFn(apiKeyProvider.Intercept))
+	}
+
+	serverBase := serverBase
+	if config.Base != "" {
+		serverBase = config.Base
+	}
+
+	client, err := NewClientWithResponses(config.Server+serverBase, clientOptions...)
+	if err != nil {
+		return nil, err
+	}
+	return &APIClient{client: client}, nil
+}
--- internal/api/v1/server.go
+++ internal/api/v1/server.go
@@ -1,7 +1,6 @@
 package v1
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/getkin/kin-openapi/openapi3"
@@ -12,6 +11,7 @@ import (
 )
 
 //go:generate oapi-codegen -config ../schemas/v1.config.yaml ../schemas/v1.yaml
+//go:generate oapi-codegen -old-config-style -package v1 -generate client -templates ../templates -o zz_generated_extensions.go ../schemas/v1.yaml
 
 var _ ServerInterface = &Server{}
 
@@ -29,37 +29,13 @@ func NewServer(url string, consulClient *api.Client, logger hclog.Logger) (http.
 
 	s := &Server{consulClient: consulClient, logger: logger}
 	r := chi.NewRouter()
-	r.Use(middleware.JSONContentType, s.consulTokenMiddleware, middleware.OapiRequestValidator(spec, s.sendError))
+	r.Use(middleware.JSONContentType, s.consulTokenMiddleware, middleware.OapiRequestValidator(spec, sendError))
 
 	return HandlerWithOptions(s, ChiServerOptions{
 		BaseURL:    url,
 		BaseRouter: r,
 		ErrorHandlerFunc: func(w http.ResponseWriter, r *http.Request, err error) {
-			s.sendError(w, http.StatusBadRequest, err.Error())
+			sendError(w, http.StatusBadRequest, err.Error())
 		},
 	}), nil
-}
-
-func (s *Server) sendError(w http.ResponseWriter, statusCode int, msg string) {
-	w.WriteHeader(statusCode)
-	if err := json.NewEncoder(w).Encode(&Error{Message: msg}); err != nil {
-		s.logger.Error("sending error message", "error", err)
-	}
-}
-
-func sendError(w http.ResponseWriter, code int, message string) {
-	send(w, code, Error{
-		Code:    int32(code),
-		Message: message,
-	})
-}
-
-func send(w http.ResponseWriter, code int, object interface{}) {
-	w.WriteHeader(code)
-	json.NewEncoder(w).Encode(object)
-}
-
-func sendEmpty(w http.ResponseWriter, code int) {
-	w.WriteHeader(code)
-	w.Write([]byte("{}\n"))
 }
