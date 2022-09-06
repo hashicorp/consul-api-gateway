36aedf974b49d49b1195fe2a78f50243059d1d52
Andrew Stucki <andrew.stucki@hashicorp.com>
2022-09-06 16:26:46 -0400

    Add stubs for gateway client (#344)
    * Add stubs for gateway client
    
    * Fix Create generation
    
    * Update test from merge

--- internal/api/v1/server.go
+++ internal/api/v1/server.go
@@ -1,7 +1,6 @@
 package v1
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/getkin/kin-openapi/openapi3"
@@ -12,6 +11,7 @@ import (
 )
 
 //go:generate oapi-codegen -config ../schemas/v1.config.yaml ../schemas/v1.yaml
+//go:generate oapi-codegen -old-config-style -package v1 -generate client -templates ../templates -o zz_generated_extensions.go ../schemas/v1.yaml
 
 var _ ServerInterface = &Server{}
 
@@ -37,21 +37,4 @@ func NewServer(url string, consulClient *api.Client, logger hclog.Logger) http.H
 			sendError(w, http.StatusBadRequest, err.Error())
 		},
 	})
-}
-
-func sendError(w http.ResponseWriter, code int, message string) {
-	send(w, code, Error{
-		Code:    int32(code),
-		Message: message,
-	})
-}
-
-func send(w http.ResponseWriter, code int, object interface{}) {
-	w.WriteHeader(code)
-	json.NewEncoder(w).Encode(object)
-}
-
-func sendEmpty(w http.ResponseWriter, code int) {
-	w.WriteHeader(code)
-	w.Write([]byte("{}\n"))
 }
