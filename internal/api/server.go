f3c8eadbd25140c47e9c24b1249eaae46622c74b
Mike Morris <mikemorris@users.noreply.github.com>
2022-09-15 17:36:17 -0400

    api: use chi/v5 everywhere (#372)

--- internal/api/server.go
+++ internal/api/server.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5"
 	"github.com/hashicorp/consul-api-gateway/internal/api/apiinternal"
 	v1 "github.com/hashicorp/consul-api-gateway/internal/api/v1"
 	consul "github.com/hashicorp/consul/api"
